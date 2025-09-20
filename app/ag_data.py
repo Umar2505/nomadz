12643162d5145e73a7943538b859d73550daa3c7
Umar Rustamov <rustamovumar0@gmail.com>
2025-09-20 17:06:33 +0500

    added

--- app/ag_data.py
+++ app/ag_data.py
@@ -1,6 +1,9 @@
-from tools import retriever_data, sanitizer
+from tools import retriever_data, sanitizer, violation_corrector
 import getpass
 import os
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
 
 if not os.environ.get("GROQ_API_KEY"):
     os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for Groq: ")
@@ -9,15 +12,5 @@ from langchain.chat_models import init_chat_model
 
 model = init_chat_model("allam-2-7b", model_provider="groq")
 
-from langchain.chat_models import init_chat_model
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.prebuilt import create_react_agent
-
-# Create the agent
-memory = MemorySaver()
-retrieve = retriever_data()
-sanitize = sanitizer()
-tools = [retrieve, sanitize]
-# tools = []
-agent_executor = create_react_agent(model, tools, checkpointer=memory)
-
+tools = [violation_corrector, retriever_data, sanitizer]
+agent_executor = create_react_agent(llm=model, tools=tools)
